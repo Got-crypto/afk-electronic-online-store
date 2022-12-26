fb7c4ab0289ed6cef7cdc28f72350fb5adba9d77
Got-crypto <pecpalmplay@gmail.com>
2022-12-26 05:17:59 +0200

    created context to handle all dynamics in the entire app

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -5,6 +5,7 @@ import { client, urlFor } from '../../lib/client'
 
 import { Product } from '../../components'
 
+import { useStateContext } from '../../context/StateContext'
 
 export const getStaticPaths = async () => {
     const query = `*[_type == "product"] {
@@ -46,6 +47,8 @@ export const getStaticProps = async ({params: { slug }}) => {
 const ProductDetails = ({product, products}) => {
 
     const [index, setIndex] = useState(0)
+
+    const { decQty, incQty, qty, onAdd } = useStateContext()
   
     const { image, name, details, price } = product
     return (
@@ -90,19 +93,19 @@ const ProductDetails = ({product, products}) => {
                         <div className='quantity'>
                             <h3>Quantity:</h3>
                             <p className='quantity-desc'>
-                                <span className='minus' onClick="">
+                                <span className='minus' onClick={decQty}>
                                     <AiOutlineMinus />
                                 </span>
-                                <span className='num' onClick="">
-                                    0
+                                <span className='num' >
+                                    {qty}
                                 </span>
-                                <span className='plus' onClick="">
+                                <span className='plus' onClick={incQty}>
                                     <AiOutlinePlus />
                                 </span>
                             </p>
                         </div>
                         <div className='buttons'>
-                            <button type='button' className='add-to-cart' onClick=''>
+                            <button type='button' className='add-to-cart' onClick={()=> onAdd(product, qty)}>
                                 Add to Cart
                             </button>
                             <button type='button' className='buy-now' onClick=''>
